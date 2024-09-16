c4f2f29b2c76114696de936540d0b3ae826fa60b
Wellington Barbosa de Jesus <wellington_ueg@hotmail.com>
2024-09-15 22:49:52 -0300

    Atualizações no projeto e criação do arquivo .gitignore

--- app/nlp.py
+++
@@ -1,7 +0,0 @@
-def gerar_sql(pergunta):
-    if "total de pacientes" in pergunta.lower():
-        return "SELECT COUNT(*) FROM tasy.pacientes"
-    elif "nome dos pacientes" in pergunta.lower():
-        return "SELECT nome FROM tasy.pacientes WHERE ROWNUM <= 10"
-    else:
-        return None
